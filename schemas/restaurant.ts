68d967cb1f12e9d38ae4f0bf45438770716d095d
yeison2017 <yha.big@gmail.com>
2023-02-13 17:21:13 -0500

    Se crea el shema para restaurant

---
+++ schemas/restaurant.ts
@@ -0,0 +1,60 @@
+export default {
+  name: 'restaurant',
+  type: 'document',
+  title: 'Restaurant',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Restaurant name',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'short_description',
+      type: 'string',
+      title: 'Short description',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image of the Restaurant',
+    },
+    {
+      name: 'lat',
+      type: 'number',
+      title: 'Latitude of the Restaurant',
+    },
+    {
+      name: 'long',
+      type: 'number',
+      title: 'Longitude of the Restaurant',
+    },
+    {
+      name: 'address',
+      type: 'string',
+      title: 'Reservations address',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'rating',
+      type: 'number',
+      title: 'Enter a Rating from (1-5 Stars)',
+      validation: (Rule: any) =>
+        Rule.required().min(1).max(1).error('Please enter a Value between 1 and 5'),
+    },
+    {
+      name: 'type',
+      title: 'Category',
+      validation: (Rule: any) => Rule.required(),
+      type: 'reference',
+      to: [{type: 'category'}],
+    },
+    {
+      name: 'dishes',
+      type: 'array',
+      title: 'Dishes',
+      of: [{type: 'reference', to: [{type: 'dish'}]}],
+    },
+  ],
+}
